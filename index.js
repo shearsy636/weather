24e267273c6486afffca29e818290d6b4cf387cd
shearsy636 <36412666+shearsy636@users.noreply.github.com>
2018-03-09 10:54:12 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var addF = document.getElementById("add32");
+
+addF.onclick = function() {
+  let tempResult = parseInt(document.getElementById.("fieldname").value) + 32;
+  result.innerHTML = tempResult;
+};
